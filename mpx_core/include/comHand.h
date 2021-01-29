0d48a03e140f5bbfc6cc74bd880f247da6946e5e
Iphares <70335690+Iphares@users.noreply.github.com>
2021-01-29 08:59:42 -0500

    Add files via upload

---
+++ mpx_core/include/comHand.h
@@ -0,0 +1,4 @@
+int comHand(char letter);
+
+
+
---
+++ mpx_core/include/userFunctions.h
@@ -0,0 +1,13 @@
+
+	void SetTime(int hours, int minutes, int seconds);		
+	int GetTime();	
+	void Setdate(int day, int month, int year);	
+	int BCDtoDec(unsigned char value);	
+	int GetDate();
+	void Version();
+	void Help(char* request);
+	printf(char msg[]);	
+	
+	
+	
+	
