ad5773c7fada4457fe217e5df561b7e56359b4c6
Schmanter <gap0010@mix.wvu.edu>
2021-02-04 19:14:06 -0500

    comment test 2

--- mpx_core/modules/R1/userFunctions.h
+++ mpx_core/modules/R1/userFunctions.h
@@ -1,11 +1,19 @@
 
-	void SetTime(int hours, int minutes, int seconds);		
-	int GetTime();	
-	void Setdate(int day, int month, int year);	
-	int BCDtoDec(unsigned char value);	
+	//! Sets the time of the system
+	void SetTime(int hours, int minutes, int seconds);
+	//! Gets the time of the system
+	int GetTime();
+	//! Sets the date of the system
+	void Setdate(int day, int month, int year);
+	//! Changes Binary Converted Decimal to Decimal
+	int BCDtoDec(unsigned char value);
+	//! Gets the date of the system
 	int GetDate();
+	//! States the version of the system
 	void Version();
+	//! Gives help for another function
 	void Help(char* request);
+	//! Prints
 	void printf(char msg[]);
+	//! Changes an uppercase char to a lowercase char
 	char toLowercase(char c);
-	
