399f33cfdd457b06541683884cfdae81d0f952e0
Schmanter <gap0010@mix.wvu.edu>
2021-02-08 16:37:45 -0500

    Commented

--- mpx_core/modules/R1/userFunctions.c
+++ mpx_core/modules/R1/userFunctions.c
@@ -1,10 +1,12 @@
-/*
-  ----- userFunctions.c -----
-  Description..: Includes all commands the user has access to along with their supporting functions.
 
-      Initial Implementation -- by Isiah Phares, 2021.
-      Modifications by:    Morgan Bartley 2021, Jordan Potter 2021, Greg Potts 2021.
-*/
+/// Description: Includes all commands the user has access to along with their supporting functions.
+///
+/// Functions within userFunctions:
+///     itoa, BCDtoDec, DectoBCD, SetTime, GetTime, SetDate, GetDate, version, toLowercase, help
+///
+///
+/// Initial Implementation -- by Isiah Phares, 2021.
+/// Modifications by:    Morgan Bartley 2021, Jordan Potter 2021, Greg Potts 2021.
 
 #include <stdint.h>
 #include <string.h>
@@ -15,19 +17,9 @@
 #include "../mpx_supt.h"
 #include "userFunctions.h"
 
-/// Description for Class userFunctions
-///
-///
-
-   /*
-	function: itoa
-	Description: An integer is taken and seperated into individual chars and then all placed into a character array. Adapted from geeksforgeeks.org.
-  */
-  /// Brief Description of itoa
+  /// Description: An integer is taken and seperated into individual chars and then all placed into a character array. Adapted from geeksforgeeks.org.
   ///
-  /// Detailed Description
-  /// @param num This is an integer
-  ///
+  /// @param num integer to be put into array
   ///
   char* itoa(int num)
 	{
@@ -57,19 +49,16 @@
 		return(char*)arr1;
 	}
 
-
-	/*
-		function: BCDtoDec
-		Description: Chnages binary number to decimal numbers.
-	*/
+	/// Description: Changes binary number to decimal numbers.
+  ///
+  /// @param value Binary number to be changed to decimal
 	int BCDtoDec(unsigned char value)	{
 		return(value-6*(value>>4));
 	}
 
-	/*
-		function: DectoBCD
-		Description: Changes decimal numbers to binary numbers.
-	*/
+  /// Description: Changes decimal numbers to binary numbers.
+  ///
+  /// @param Decimal Decimal number to be changed to binary
 	int  DectoBCD (int Decimal)	{
   		 return (((Decimal/10) << 4) | (Decimal % 10));
 	}
@@ -78,12 +67,11 @@
 		sys_req(WRITE, COM1, msg, (int*)strlen(msg));
 	}
 
-
-	/*
-		function: SetTime
-		Description: sets the time register to the new values that the user inputed, all values must be inputed
-		as SetTime(Hours, Minutes, Seconds).
-	*/
+  /// Description: sets the time register to the new values that the user inputed, all values must be inputed as SetTime(Hours, Minutes, Seconds).
+  ///
+  /// @param hours Integer to be set in the Hour position
+  /// @param minutes Integer to be set in the Minutes position
+  /// @param seconds Integer to be set in the Seconds position
 	void SetTime(int hours, int minutes, int seconds)	{
 
 		cli(); //outb(device + 1, 0x00); //disable interrupts
@@ -96,12 +84,9 @@
 		sti();  //outb(device + 4, 0x0B); //enable interrupts, rts/dsr set
 	}
 
-
-	/*
-		function: GetTime
-		Description: retrieve and return the time values for hours, minutes, and seconds form the clock
-		register using inb(Port,address).
-	*/
+  /// Description: retrieve and return the time values for hours, minutes, and seconds form the clock register using inb(Port,address).
+  ///
+  /// No parameters.
   void GetTime()	{
 	int hour;
 	int minute;
@@ -122,11 +107,12 @@
 		sys_req(WRITE, COM1, itoa(second), (int*)2);
 	}
 
-
-	/*
-		function: SetDate
-		Description:
-	*/
+  /// Description: Sets the date register to the new values that the user inputed, all values must be inputed as SetDime(day, month, millenial, year).
+  ///
+  /// @param day Integer to be set in the Day position
+  /// @param month Integer to be set in the Month position
+  /// @param millenial Integer to be set in the Millenial position
+  /// @param year Integer to be set in the Year position
   void SetDate(int day, int month, int millennial, int year)	{
 		cli();
 		outb(0x70,0x07);
@@ -143,10 +129,9 @@
 		sti();
 	}
 
-	/*
-		function: GetDate
-		Description: Returns the full date back to the user in decimal form.
-	*/
+  /// Description: Returns the full date back to the user in decimal form.
+  ///
+  /// No parameters.
   void GetDate()	{
 		unsigned char day = BCDtoDec(inb(0x07));
 		unsigned char month = BCDtoDec(inb(0x08));
@@ -163,22 +148,17 @@
 		sys_req(WRITE, COM1, itoa(year), (int*)2);
 	}
 
-
-
-	/*
-		function: Version
-		Description: Simply returns a char containing
-		"Version: R(module).(the iteration that module is currently on).
-	*/
+  /// Description: Simply returns a char containing "Version: R(module).(the iteration that module is currently on).
+  ///
+  /// No parameters.
 	void Version()	{
 		//char msg[13]="Version: R1.1";
 		sys_req(WRITE, COM1, "Version: R1.1", (int*)13 );
 	}
 
-	/*
-		function: toLowercase
-		Description: If a letter is uppercase, it changes it to lowercase. (char)
-	*/
+  /// Description: If a letter is uppercase, it changes it to lowercase. (char)
+  ///
+  /// @param c Character that is to be changed to its lowercase equivalent
 	char toLowercase(char c)	{
    		if((c >= 65) && (c <= 90))	{
         		c = c + 32;
@@ -186,12 +166,11 @@
     	return c;
     	}
 
-	/*
-		function: Help
-		Description: Can except a string as a pointer, if the pointer is null then the function will print a complete list of avaliable commands
-		to the console. If the pointer is a avaliable commands then instructions on how to use the command will be printed.
-		If the command does not exist then a message explaining that it is not a valid command will be displayed.
-	*/
+  /// Brief Description: Gives helpful information for one of the functions
+  ///
+  /// Description: Can except a string as a pointer, if the pointer is null then the function will print a complete list of avaliable commands to the console. If the pointer is a avaliable commands then instructions on how to use the command will be printed. If the command does not exist then a message explaining that it is not a valid command will be displayed.
+  ///
+  /// @param request Character pointer that matches the name of the function that you need help with.
   void Help(char* request)	{
 		if (*request == '\0')	{
 			//char msg[100]="\n GetDate \n SetDate \n GetTime \n SetTime \n Version \n";
