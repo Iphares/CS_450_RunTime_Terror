dc14703a39eed6eec22048801e6cfc91fe4d84aa
Iphares <70335690+Iphares@users.noreply.github.com>
2021-02-11 19:31:24 -0500

    Update userFunctions.c

--- mpx_core/modules/R1/userFunctions.c
+++ mpx_core/modules/R1/userFunctions.c
@@ -61,8 +61,8 @@
 	/// Description: Changes binary number to decimal numbers.
   ///
   /// @param value Binary number to be changed to decimal
-	int BCDtoDec(unsigned char value)	{
-		return(value-6*(value>>4));
+	int BCDtoDec(int BCD)	{
+  		return (((BCD>>4)*10) + (BCD & 0xF))
 	}
 
   /// Description: Changes decimal numbers to binary numbers.
