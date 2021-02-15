3eae777a31f871570afac57785eb1d89c09ef8bb
Iphares <70335690+Iphares@users.noreply.github.com>
2021-02-15 18:19:17 -0500

    Create PCB.h

---
+++ mpx_core/modules/R1/PCB.h
@@ -0,0 +1,36 @@
+
+
+#DEFINE 1KMEM 1024;
+#DEFINE 2KMEM 2048;
+#DEFINE 3KMEM 4096;
+#DEFINE 4KMEM 8192;
+#DEFINE 5KMEM 16384;
+
+typedef struct Queue	{
+    
+        int count;
+        PCB *head
+        PCB *tail
+}
+typedef struct PCB	{
+	char[1KMEM] stack;
+	char[10] Process_Name;
+	int Process_Class;
+        int Priority;
+        int State;
+        int Process_Stack;
+        int *PCB_Pointer;
+}
+
+
+
+
+
+
+
+Queue AllocatePCB();
+char FreePCB(*PCB);
+PCB SetupPCB(char[] Name, int Class, int Level );
+PCB FindPCB(char[] Name);
+InsertPCBA(	);
+RemovePCB( );
