9a8bfe20c9fefe15e082071c2440a25d70306367
Iphares <70335690+Iphares@users.noreply.github.com>
2021-02-22 22:32:52 -0500

    Update comHand.c

--- mpx_core/modules/R1/comHand.c
+++ mpx_core/modules/R1/comHand.c
@@ -1,4 +1,4 @@
-/// @file
+// @file
 
 /*
   ----- comHand.c -----
@@ -36,159 +36,163 @@
 			char* ThirdToken = strtok(NULL, "-");
 			char* FourthToken = strtok(NULL, "-");
 			char* FifthToken = strtok(NULL, "-");
-			if(shutdown == 0){
+			if(shutdown == 0)	{
 /************************************************************************************
 				R1 comHand
 ************************************************************************************/
-				if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,NULL) == 0)		{
-					Help("\0");
-				}
-				//R1 Commands
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"version") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("Version");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"getDate") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("GetDate");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"setDate") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("SetDate");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"getTime") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("GetTime");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"setTime") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("SetTime");
-				}
-				// R2 Commands
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"suspend") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("suspend");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"resume") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("resume");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"setPriority") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("setPriority");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showPCB") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("showPCB");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showAll") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("showAll");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showReady") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("showReady");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showBlocked") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("showBlocked");
-				}
-				// Temporary R2 commands
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"createPCB") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("createPCB");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"deletePCB") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("deletePCB");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"block") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("block");
-				}
-				else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"unblock") == 0 && strcmp(ThirdToken,NULL) == 0)	{
-					Help("unblock");
-				}
+					if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,NULL) == 0)		{
+						Help("\0");
+					}
+					//R1 Commands
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"version") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("Version");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"getDate") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("GetDate");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"setDate") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("SetDate");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"getTime") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("GetTime");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"setTime") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("SetTime");
+					}
+					// R2 Commands
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"suspend") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("suspend");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"resume") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("resume");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"setPriority") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("setPriority");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showPCB") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("showPCB");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showAll") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("showAll");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showReady") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("showReady");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"showBlocked") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("showBlocked");
+					}
+					// Temporary R2 commands
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"createPCB") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("createPCB");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"deletePCB") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("deletePCB");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"block") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("block");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"unblock") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("unblock");
+					}
+					else if(strcmp(FirstToken,"help") == 0 && strcmp(SecondToken,"shutdown") == 0 && strcmp(ThirdToken,NULL) == 0)	{
+						Help("shutdown");
+					}
 
-				else if(strcmp(FirstToken,"version") == 0 && strcmp(SecondToken,NULL) == 0)
-					Version();
+					else if(strcmp(FirstToken,"version") == 0 && strcmp(SecondToken,NULL) == 0)
+						Version();
 
-				else if(strcmp(FirstToken,"getDate") == 0 && strcmp(SecondToken,NULL) == 0)
-					GetDate();
+					else if(strcmp(FirstToken,"getDate") == 0 && strcmp(SecondToken,NULL) == 0)
+						GetDate();
 
-				else if(strcmp(FirstToken,"setDate") == 0){
-					if (EdgeCase(SecondToken) == 1 && EdgeCase(ThirdToken) == 1 && EdgeCase(FourthToken) == 1 && EdgeCase(FifthToken) == 1)    {
-					    SetDate(atoi(SecondToken), atoi(ThirdToken), atoi(FourthToken), atoi(FifthToken));
+					else if(strcmp(FirstToken,"setDate") == 0){
+						if (EdgeCase(SecondToken) == 1 && EdgeCase(ThirdToken) == 1 && EdgeCase(FourthToken) == 1 && EdgeCase(FifthToken) == 1)    {
+						    SetDate(atoi(SecondToken), atoi(ThirdToken), atoi(FourthToken), atoi(FifthToken));
+						}
+						else
+							printf("\x1b[31m""\nERROR: Invalid parameters for setDate \n""\x1b[0m");
+					}
+					else if(strcmp(FirstToken,"getTime") == 0 && strcmp(SecondToken,NULL) == 0)	//Return the current time held by the registers.
+						GetTime();
+					else if(strcmp(FirstToken,"setTime") == 0 && strcmp(FifthToken,NULL) == 0){
+						if (EdgeCase(SecondToken) == 1 && EdgeCase(ThirdToken) == 1 && EdgeCase(FourthToken) == 1)    {
+								SetTime(atoi(SecondToken), atoi(ThirdToken), atoi(FourthToken));		//input as Hour-Minute-Seconds
+						}
+						else
+							printf("\x1b[31m""\nERROR: Invalid parameters for setTime \n""\x1b[0m");
+					}
+	/************************************************************************************
+					R2 comHand
+	************************************************************************************/
+					else if(strcmp(FirstToken,"suspend") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Suspend(SecondToken);
+					}
+					else if(strcmp(FirstToken,"resume") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Resume(SecondToken);
+					}
+					else if(strcmp(FirstToken,"setPriority") == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						if(EdgeCase(ThirdToken) == 1)	{
+							Set_Priority(SecondToken, atoi(ThirdToken));			//input as setPriority-Process_Name-Priority
+						}
+						else
+							printf("\x1b[31m""\nERROR: Invalid parameters for setPriority, priority must be entered as a integer. \n""\x1b[0m");
+					}
+					else if(strcmp(FirstToken,"showPCB") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Show_PCB(SecondToken);
+					}
+					else if(strcmp(FirstToken,"showAll") == 0 && strcmp(SecondToken,NULL) == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Show_All();
+					}
+					else if(strcmp(FirstToken,"showReady") == 0 && strcmp(SecondToken,NULL) == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Show_Ready();
+					}
+					else if(strcmp(FirstToken,"showBlocked") == 0 && strcmp(SecondToken,NULL) == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Show_Blocked();
+					}
+
+					/********** R2 Temp Commands **********/
+					else if(strcmp(FirstToken,"createPCB") == 0) {
+						if( strlen(SecondToken) < 11)	{
+							// if(EdgeCase(ThirdToken) == 1 && EdgeCase(FourthToken) == 1)    {
+								Create_PCB(SecondToken, atoi(ThirdToken), atoi(FourthToken));		//input as Process_Name-Priority-Class
+							// }
+							// else
+								// printf("\x1b[31m""\nERROR: Invalid parameters for createPCB, class and priority must be entered as integers. \n""\x1b[0m");
+						}
+						else
+							printf("\x1b[31m""\nERROR: Invalid parameters for createPCB, Process_name must only contain 10 or fewer characters. \n""\x1b[0m");
+					}
+					else if(strcmp(FirstToken,"deletePCB") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Delete_PCB(SecondToken);
+					}
+					else if(strcmp(FirstToken,"block") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Block(SecondToken);
+					}
+					else if(strcmp(FirstToken,"unblock") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
+						Unblock(SecondToken);
+					}
+
+	/************************************************************************************
+					shutdown comHand
+	************************************************************************************/
+					else if(strcmp(FirstToken,"shutdown") == 0 && strcmp(SecondToken,NULL) == 0){
+						printf("\x1b[33m""\nAre you sure you want to shutdown? [yes/no]\n""\x1b[0m");
+						shutdown = 1;
+					}
+					else	{
+						printf("\x1b[31m""\nERROR: Not a valid command \n""\x1b[0m");
+					}
+				}
+				else{
+					if(strcmp(FirstToken,"yes") == 0 && shutdown == 1)	{
+						quit = 1;
+					}
+					else if(strcmp(FirstToken,"no") == 0){
+						printf("\x1b[33m""\nShutdown Cancelled\x1b[0m \n");
+						shutdown = 0;
 					}
 					else
-						printf("\x1b[31m""\nERROR: Invalid parameters for setDate \n""\x1b[0m");
+						printf("\x1b[31m""\nERROR: Please enter \"yes\" or \"no\" \n""\x1b[0m");
 				}
-				else if(strcmp(FirstToken,"getTime") == 0 && strcmp(SecondToken,NULL) == 0)	//Return the current time held by the registers.
-					GetTime();
-				else if(strcmp(FirstToken,"setTime") == 0 && strcmp(FifthToken,NULL) == 0){
-					if (EdgeCase(SecondToken) == 1 && EdgeCase(ThirdToken) == 1 && EdgeCase(FourthToken) == 1)    {
-							SetTime(atoi(SecondToken), atoi(ThirdToken), atoi(FourthToken));		//input as Hour-Minute-Seconds
-					}
-					else
-						printf("\x1b[31m""\nERROR: Invalid parameters for setTime \n""\x1b[0m");
-				}
-/************************************************************************************
-				R2 comHand
-************************************************************************************/
-				if(strcmp(FirstToken,"suspend") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Suspend(SecondToken);
-				}
-				else if(strcmp(FirstToken,"resume") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Resume(SecondToken);
-				}
-				else if(strcmp(FirstToken,"setPriority") == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					if(EdgeCase(ThirdToken) == 1)	{
-						Set_Priority(SecondToken, atoi(ThirdToken));			//input as setPriority-Process_Name-Priority
-					}
-					else
-						printf("\x1b[31m""\nERROR: Invalid parameters for setPriority, priority must be entered as a integer. \n""\x1b[0m");
-				}
-				else if(strcmp(FirstToken,"showPCB") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Show_PCB(SecondToken);
-				}
-				else if(strcmp(FirstToken,"showAll") == 0 && strcmp(SecondToken,NULL) == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Show_All();
-				}
-				else if(strcmp(FirstToken,"showReady") == 0 && strcmp(SecondToken,NULL) == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Show_Ready();
-				}
-				else if(strcmp(FirstToken,"showBlocked") == 0 && strcmp(SecondToken,NULL) == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Show_Blocked();
-				}
-
-				/********** R2 Temp Commands **********/
-				else if(strcmp(FirstToken,"createPCB") == 0) {
-					if( strlen(SecondToken) < 11)	{
-						// if(EdgeCase(ThirdToken) == 1 && EdgeCase(FourthToken) == 1)    {
-							Create_PCB(SecondToken, atoi(ThirdToken), atoi(FourthToken));		//input as Process_Name-Priority-Class
-						// }
-						// else
-							// printf("\x1b[31m""\nERROR: Invalid parameters for createPCB, class and priority must be entered as integers. \n""\x1b[0m");
-					}
-					else
-						printf("\x1b[31m""\nERROR: Invalid parameters for createPCB, Process_name must only contain 10 or fewer characters. \n""\x1b[0m");
-				}
-				else if(strcmp(FirstToken,"deletePCB") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Delete_PCB(SecondToken);
-				}
-				else if(strcmp(FirstToken,"block") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Block(SecondToken);
-				}
-				else if(strcmp(FirstToken,"unblock") == 0 && strcmp(ThirdToken,NULL) == 0 && strcmp(FourthToken,NULL) == 0 && strcmp(FifthToken,NULL) == 0) {
-					Unblock(SecondToken);
-				}
-
-/************************************************************************************
-				shutdown comHand
-************************************************************************************/
-				else if(strcmp(FirstToken,"shutdown") == 0 && strcmp(SecondToken,NULL) == 0){
-					printf("\x1b[33m""\nAre you sure you want to shutdown? [yes/no]\n""\x1b[0m");
-					shutdown = 1;
-				}
-				else
-					printf("\x1b[31m""\nERROR: Not a valid command \n""\x1b[0m");
-				}
-			else{
-				if(strcmp(FirstToken,"yes") == 0 && shutdown == 1)	{
-					quit = 1;
-				}
-				else if(strcmp(FirstToken,"no") == 0){
-					printf("\x1b[33m""\nShutdown Cancelled\x1b[0m \n");
-					shutdown = 0;
-				}
-				else
-					printf("\x1b[31m""\nERROR: Please enter \"yes\" or \"no\" \n""\x1b[0m");
-			}
 		}
 		return 0;	//shutdown procedure
 	}
