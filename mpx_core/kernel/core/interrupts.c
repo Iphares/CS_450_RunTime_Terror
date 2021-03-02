215f1e2052420c96ee2dfd9dc93dacb836602a7f
Iphares <70335690+Iphares@users.noreply.github.com>
2021-03-02 12:34:21 -0500

    idt_set_gate(60

--- mpx_core/kernel/core/interrupts.c
+++ mpx_core/kernel/core/interrupts.c
@@ -96,6 +96,10 @@ void init_irq(void)
   }
   // Ignore interrupts from the real time clock
   idt_set_gate(0x08, (u32int)rtc_isr, 0x08, 0x8e);
+  
+  //R3 interrupt 60
+  idt_set_gate(60, (u32int)sys_call_isr, 0x08, 0x8e);
+  
 }
 
 /*
