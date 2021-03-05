e565bbc5a89867c2d8613fc2a07a182461d57c40
Iphares <70335690+Iphares@users.noreply.github.com>
2021-03-05 08:47:22 -0500

    Update userFunctions.c

--- mpx_core/modules/R1/userFunctions.c
+++ mpx_core/modules/R1/userFunctions.c
@@ -919,21 +919,26 @@ void Unblock(char *ProcessName)	{
 ********************************************************************************************************/
 
 PCB loadr3()	{
-	
-	pcb * new_pcb = create_pcb ( name , 1 , 1 , 1 , stack_size );
-	context * cp = ( context *)( new_pcb -> stack_top );
-	memset ( cp , 0, sizeof ( context ));
-	cp - > fs = 0 x10 ;
-	cp - > gs = 0 x10 ;
-	cp - > ds = 0 x10 ;
-	cp - > es = 0 x10 ;
-	cp - > cs = 0 x8 ;
-	cp - > ebp = ( u32int )( new_pcb -> stack );
-	cp - > esp = ( u32int )( new_pcb -> stack_top );
-	cp - > eip = ( u32int ) proc1();// The function correlating to the process , ie. Proc1
-	cp - > eflags = 0 x202 ;
-	return new_pcb ;
-	
+	Char[] list ={"proc1", "proc2", "proc3", "proc4", "proc5"};
+	int control;
+	for(control = 0; control < 5; control++)	{
+		name = list[control];
+		
+		pcb * new_pcb = create_pcb ( name , 1 , 1 , 1 , stack_size );
+		context * cp = ( context *)( new_pcb -> stack_top );
+		memset ( cp , 0, sizeof ( context ));
+		cp - > fs = 0 x10 ;
+		cp - > gs = 0 x10 ;
+		cp - > ds = 0 x10 ;
+		cp - > es = 0 x10 ;
+		cp - > cs = 0 x8 ;
+		cp - > ebp = ( u32int )( new_pcb -> stack );
+		cp - > esp = ( u32int )( new_pcb -> stack_top );
+		cp - > eip = ( u32int ) proc1();// The function correlating to the process , ie. Proc1
+		cp - > eflags = 0 x202 ;
+		return new_pcb ;	
+		
+	}		
 }
 
 void yield()	{
