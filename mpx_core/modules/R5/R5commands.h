ae55d53c86670075c60735bcdf8b6859c2617679
Iphares <70335690+Iphares@users.noreply.github.com>
2021-04-05 20:00:33 -0400

    Update R5commands.h

--- mpx_core/modules/R5/R5commands.h
+++ mpx_core/modules/R5/R5commands.h
@@ -1,9 +1,12 @@
 //Temporary
-void Init_Heap();
+void Init_Heap(int size);
+
 //Temporary
-void Alloc_Mem();
+void Alloc_Mem(u32int size);
+
 //Temporary
-void Free_Mem();
+void Free_Mem(u32int address);
+
 //Temporary
 void IsEmpty();
 
